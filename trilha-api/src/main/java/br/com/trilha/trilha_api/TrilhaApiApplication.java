fc1dbff769e5df07191fb8e61ea340e2f9e3ce86
Alefe Patrick <alefepatrick@gmail.com>
2026-03-03 14:05:34 -0300

    Adicionando Spring-Boot

---
+++ trilha-api/src/main/java/br/com/trilha/trilha_api/TrilhaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.trilha.trilha_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrilhaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrilhaApiApplication.class, args);
+	}
+
+}
